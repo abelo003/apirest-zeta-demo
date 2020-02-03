e200f1dc80ee3ca983eaa910023fa1d9b925d307
Abelo <abelo003@gmail.com>
2020-02-03 16:24:24 -0600

    iniciando proyecto

---
+++ src/main/java/api/rest/zeta/ApirestZetaDemoApplication.java
@@ -0,0 +1,13 @@
+package api.rest.zeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestZetaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestZetaDemoApplication.class, args);
+	}
+
+}

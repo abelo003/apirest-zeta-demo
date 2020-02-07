d69ed5ae8d40e177872c10898c390186ca1d8b5b
Abel Cruz <acruzb@santander.com.mx>
2020-02-07 14:57:16 -0600

    avance

--- src/main/java/api/rest/zeta/excel/ExcelWriter.java
+++ src/main/java/api/rest/zeta/excel/ExcelWriter.java
@@ -11,13 +11,18 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import org.apache.poi.ss.util.CellRangeAddress;
 
 public class ExcelWriter {
+    
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 
-    private static String[] columns = {"Name", "Email", "Date Of Birth", "Salary"};
+    private static String[] columns = {"Error Nivelación de datos (PEDT008)", "Error Límite de cajero (MPA1014)", "Error Cambio de PAN T. Física (MPE0026)"};
     private static List<Employee> employees =  new ArrayList<>();
 
 	// Initializing employees data to insert into the excel file
@@ -45,50 +50,68 @@ public class ExcelWriter {
         CreationHelper createHelper = workbook.getCreationHelper();
 
         // Create a Sheet
-        Sheet sheet = workbook.createSheet("Employee");
+        Sheet sheet = workbook.createSheet("Reporte " + sdf.format(new Date()));
 
         // Create a Font for styling header cells
+        Font infoFont = workbook.createFont();
+        infoFont.setBold(true);
+        infoFont.setFontHeightInPoints((short) 16);
+        infoFont.setColor(IndexedColors.BLUE.getIndex());
+        
         Font headerFont = workbook.createFont();
-        headerFont.setBold(true);
-        headerFont.setFontHeightInPoints((short) 14);
+        headerFont.setFontHeightInPoints((short) 12);
         headerFont.setColor(IndexedColors.RED.getIndex());
 
         // Create a CellStyle with the font
         CellStyle headerCellStyle = workbook.createCellStyle();
         headerCellStyle.setFont(headerFont);
+        
+        CellStyle infoCellStyle = workbook.createCellStyle();
+        infoCellStyle.setFont(infoFont);
+        infoCellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        infoCellStyle.setAlignment(HorizontalAlignment.CENTER);
 
         // Create a Row
-        Row headerRow = sheet.createRow(0);
+        Row infoRow = sheet.createRow(1);
+        Cell cellInfo = infoRow.createCell(2);
+        cellInfo.setCellValue("Reporte de errores del día " + sdf.format(new Date()));
+        cellInfo.setCellStyle(infoCellStyle);
+        sheet.addMergedRegion(new CellRangeAddress( 1, 1, 2, 5));
+        
+        // Create a Row
+        Row headerRow = sheet.createRow(3);
 
         // Create cells
-        for(int i = 0; i < columns.length; i++) {
-            Cell cell = headerRow.createCell(i);
+        for(int i = 0; i < columns.length; i++ ) {
+            Cell cell = headerRow.createCell(((i + 1) * 2) - 1);
             cell.setCellValue(columns[i]);
             cell.setCellStyle(headerCellStyle);
+            //Agregar la combinacion de celdas
+//            sheet.addMergedRegion(new CellRangeAddress( 1, 1, 2, 5));
         }
 
         // Create Cell Style for formatting Date
-        CellStyle dateCellStyle = workbook.createCellStyle();
-        dateCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd-MM-yyyy"));
+//        CellStyle dateCellStyle = workbook.createCellStyle();
+//        dateCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd-MM-yyyy"));
 
         // Create Other rows and cells with employees data
-        int rowNum = 1;
-        for(Employee employee: employees) {
-            Row row = sheet.createRow(rowNum++);
-
-            row.createCell(0)
-                    .setCellValue(employee.getName());
-
-            row.createCell(1)
-                    .setCellValue(employee.getEmail());
-
-            Cell dateOfBirthCell = row.createCell(2);
-            dateOfBirthCell.setCellValue(employee.getDateOfBirth());
-            dateOfBirthCell.setCellStyle(dateCellStyle);
-
-            row.createCell(3)
-                    .setCellValue(employee.getSalary());
-        }
+//        int rowNum = 4;
+//        for(Employee employee: employees) {
+//            Row row = sheet.createRow(rowNum++);
+//
+//            row.createCell(0)
+//                    .setCellValue(employee.getName());
+//
+//            row.createCell(1)
+//                    .setCellValue(employee.getEmail());
+//
+//            Cell dateOfBirthCell = row.createCell(2);
+//            dateOfBirthCell.setCellValue(employee.getDateOfBirth());
+//            dateOfBirthCell.setCellStyle(dateCellStyle);
+//
+//            row.createCell(3)
+//                    .setCellValue(employee.getSalary());
+//        }
 
 		// Resize all columns to fit the content size
         for(int i = 0; i < columns.length; i++) {
@@ -96,7 +119,7 @@ public class ExcelWriter {
         }
 
         // Write the output to a file
-        FileOutputStream fileOut = new FileOutputStream("C:\\Users\\abelo\\Documents\\excel\\poi-generated-file.xlsx");
+        FileOutputStream fileOut = new FileOutputStream("C:\\Users\\Z048152\\Documents\\poi-generated-file.xlsx");
         workbook.write(fileOut);
         fileOut.close();
         System.out.println("done!!!");
